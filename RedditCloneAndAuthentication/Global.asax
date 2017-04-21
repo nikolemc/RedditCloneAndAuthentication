c66c6c17aefdfd290a322f060554e36e12fac2c9
nikolemc <nikolemcstanley@gmail.com>
2017-04-21 13:05:58 -0400

    first commit

---
+++ RedditCloneAndAuthentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RedditCloneAndAuthentication.MvcApplication" Language="C#" %>
---
+++ RedditCloneAndAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedditCloneAndAuthentication.Startup))]
+namespace RedditCloneAndAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
